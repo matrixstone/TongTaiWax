6881947a28a5ffbe0162ac36321ec1e14dd496a2
xuhe <xuhe@yahoo-inc.com>
2015-11-30 15:00:35 -0800

    first checkin

---
+++ flashpic5.js
@@ -0,0 +1,24 @@
+<!--
+
+var focus_width=418
+var focus_height=264
+var text_height=0
+var swf_height = focus_height+0
+var a = "center"
+var b = "b"
+
+var pics='pic/e001.jpg|pic/e002.jpg|pic/e003.jpg'
+var links=''
+var texts=''
+
+document.write('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="'+ focus_width +'" height="'+ swf_height +'">');
+document.write('<param name="allowScriptAccess" value="sameDomain">');
+document.write('<param name="movie" value="flashpic.swf">');
+document.write('<param name="quality" value="high">');
+document.write('<param name="bgcolor" value="#ffffff">');
+document.write('<param name="menu" value="false"><param name=wmode value="opaque">');
+document.write('<param name="FlashVars" value="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'&a='+a+'&isb='+b+'">');
+document.write('<embed src="flashpic.swf" wmode="opaque" FlashVars="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'&a='+a+'&isb='+b+'" menu="false" bgcolor="#ffffff" quality="high" width="'+ focus_width +'" height="'+ focus_height +'" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');
+document.write('</object>');
+
+-->
